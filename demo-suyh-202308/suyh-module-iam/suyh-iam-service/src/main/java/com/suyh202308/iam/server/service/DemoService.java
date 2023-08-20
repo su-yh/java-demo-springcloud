a2f73b6ea2602a81ad5fa50395cc9f29f2ad6513
苏云弘 <su_787910081@sina.com>
2023-08-20 18:44:50 +0800

    添加flywaydb

--- demo-suyh-202308/suyh-module-iam/suyh-iam-service/src/main/java/com/suyh202308/iam/server/service/DemoService.java
+++ demo-suyh-202308/suyh-module-iam/suyh-iam-service/src/main/java/com/suyh202308/iam/server/service/DemoService.java
@@ -1,12 +1,20 @@
 package com.suyh202308.iam.server.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
 @Service
-//@Slf4j
+@Slf4j
 public class DemoService {
-//    @PostConstruct
-//    public void init() {
-//        log.info("demo service 启动了。");
-//    }
+    @Resource
+    private DataSource dataSource;
+
+    @PostConstruct
+    public void init() {
+        log.info("demo service 启动了。");
+    }
 }
